b67314e95d9342da1d40f42c833a4f5050ef389c
Jaco Kotzee <jaco@doodlesdigital.co.za>
2018-05-17 08:05:35 +0200

    add better comments AND added random speed of enemies

--- js/app.js
+++ js/app.js
@@ -2,27 +2,22 @@
 class Enemy {
 
     constructor(x, y, speed) {
-        // Variables applied to each of our instances go here,
-        // we've provided one for you to get started
         this.x = x;
         this.y = y;
         this.speed = speed;
-        // The image/sprite for our enemies, this uses
-        // a helper we've provided to easily load images
+        // The image/sprite for our enemies
         this.sprite = 'images/enemy-bug.png';
     }
-    // Update the enemy's position, required method for game
+    // Update the enemy's position
     // Parameter: dt, a time delta between ticks
     update(dt) {
-        // You should multiply any movement by the dt parameter
-        // which will ensure the game runs at the same speed for
-        // all computers.
+        // multiply any movement by the dt parameter
         this.x += this.speed * dt;
 
         if (this.x > 500) {
             this.x = -100;
+            this.speed = (Math.random() * (500 - 200) + 200);
         }
-
         // Check for collision between player and enemies
         if (player.x < this.x + 55
             && player.x + 40 > this.x
@@ -33,22 +28,25 @@ class Enemy {
             player.y = 400;
         }
     }
-    // Draw the enemy on the screen, required method for game
+    // Draw the enemy on the screen
     render() {
         ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
     }
 }
-const enemy1 = new Enemy(0, 50, 200);
-const enemy2 = new Enemy(0, 140, 350);
-const enemy3 = new Enemy(0, 220, 300);
+// Create enemy objects
+const enemy1 = new Enemy(0, 50, (Math.random() * (500 - 200) + 200));
+const enemy2 = new Enemy(0, 140, (Math.random() * (500 - 200) + 200));
+const enemy3 = new Enemy(0, 220, (Math.random() * (500 - 200) + 200));
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
 class Player {
+
     constructor(x, y) {
         this.x = x;
         this.y = y;
+        // The image/sprite for our player
         this.sprite = 'images/char-boy.png';
     }
 
@@ -65,9 +63,11 @@ class Player {
             this.y = 400;
         }
     }
+    // Draw the player on the screen
     render() {
         ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
     }
+    // When arrows are pressed, it moves player position
     handleInput(e) {
 
         if (e === 'up') {
@@ -82,21 +82,17 @@ class Player {
     }
 }
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
+// instantiate objects.
+// all enemy objects placed in an array
 const allEnemies = [];
 allEnemies.push(enemy1, enemy2, enemy3);
 
-// Place the player object in a variable called player
+// the player object placed in a variable
 const player = new Player(200, 400);
 
-
-
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+// This listens for key presses and sends the keys to Player.handleInput() method.
 document.addEventListener('keyup', function (e) {
 
-    // console.log('key press');
     var allowedKeys = {
         37: 'left',
         38: 'up',
